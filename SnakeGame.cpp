b691610d6d4b5394ad7c489a18de076222642924
Daniel Bartolini <djb3149@gmail.com>
2023-05-24 00:24:36 -0400

    add tail logic

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -4,26 +4,32 @@
 #include "ncurses.h"// refresh
 
 bool gameOver;
+// bounds for game
 const int width = 30;
 const int height = 30;
+// game variables
 int x, y, fruitX, fruitY;
+int tailX[100], tailY[100];
+int nTail;
 int score;
 enum eDir { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDir dir;
 
-using namespace std;
-
 void Setup() {
+  // ncurses initialization
   initscr();
   cbreak();
   noecho();
   curs_set(0);
   
   gameOver = false;
+
+  // initialize new snake (player) in center of map
   dir = STOP;
   x = width / 2;
   y= height / 2;
   
+  // random starting fruit
   fruitX = rand() % width;
   fruitY = rand() % height;
   
@@ -33,10 +39,14 @@ void Setup() {
 void Draw() {
   clear();
  
+  // draw the map boundaries
+  // top boundary `#`
   for (int i = 0; i < width; i++) {
     mvprintw(0, i, "#");
   }
-
+  // middle portion
+  // can contain boundary `#`, snake head `O`, snake tail `o`, or fruit `X`
+  // checks the current value of each and draws window accordingly
   for (int i = 1; i < height - 1; i++) {
     mvprintw(i, 0, "#");
     for (int j = 1; j < width - 1; j++) {
@@ -46,21 +56,34 @@ void Draw() {
         mvprintw(i, j, "O");
       } else if ((i - 1) == fruitY && (j - 1) == fruitX) {
         mvprintw(i, j, "X");
+      } else {
+        bool isTailSegment = false;
+        for (int k = 0; k < nTail; k++) {
+          if (tailX[k] == j && tailY[k] == i) {
+            mvprintw(i, j, "o");
+            isTailSegment = true;
+            break;
+          }
+        }
+        if (!isTailSegment) {
+          mvprintw(i, j, " ");
+        }
       }
-
     }
     mvprintw(i, width - 1, "#");
   }
-
+  // bottom boundary `#`
   for (int i = 0; i < width; i++) {
     mvprintw(height - 1, i, "#");
   }
   
+  // print score
   mvprintw(0, width + 2, "Score: %d", score);
+
   refresh();
 }
 
-
+// use `ncurses` for keyboard inputs
 void Input() {
   int ch = getch();
 
@@ -83,7 +106,25 @@ void Input() {
   }
 }
 
+// game logic
 void Logic() {
+  // tail position made by finding the previous 2 tails coordinates
+  int prevX = tailX[0];
+  int prevY = tailY[0];
+  int prev2X, prev2Y;
+  tailX[0] = x + 1;
+  tailY[0] = y + 1;
+
+  for (int i = 1; i < nTail; i++) {
+    prev2X = tailX[i];
+    prev2Y = tailY[i];
+    tailX[i] = prevX;
+    tailY[i] = prevY;
+    prevX = prev2X;
+    prevY = prev2Y;
+  }
+
+  // 'listen' for key inputs
   switch (dir) {
     case LEFT:
       x--;
@@ -100,17 +141,31 @@ void Logic() {
     default:
       break;
   }
-  if (x > width || x < 0 || y > height || y < 0) {
+
+  // you die if you hit the boundary
+  if (x >= width || x < 0 || y >= height || y < 0) {
     gameOver = true;
   }
+
+//  // you die if you eat your tail
+//  for (int i = 0; i < nTail; i++) {
+//    if (tailX[i] == x && tailY[i] == y) {
+//      gameOver = true;
+//    }
+//  }
+
+  // your score increases and tail grows for every fruit eaten
   if (x == fruitX && y == fruitY) {
     score += 10;
 
     fruitX = rand() % width;
     fruitY = rand() % height;
+
+    nTail++;
   }
 }
 
+// cleanup ncurses functions
 void Cleanup() {
   endwin();
 }
